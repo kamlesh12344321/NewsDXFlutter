b9f426794346031d2c9856dda06ff131f58fee9f
kamlesh <kamlesh.p@ninestars.in>
2022-06-10 12:43:05 +0530

    article detail screen widget

---
+++ lib/screens/article_detail.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ArticleDetail extends StatelessWidget {
+  const ArticleDetail({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
