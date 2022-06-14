a0bb4a0e863cd3c0506000dcdd3286517d3b8412
kamlesh <pkamlesh0124@gmail.com>
2022-06-14 14:58:58 +0530

    add dependented file on google, facebook and apple login

---
+++ lib/subscription/subscriprtion_plan_screen.dart
@@ -0,0 +1,23 @@
+
+import 'package:flutter/material.dart';
+
+class SubscriptionPlanScreen extends StatefulWidget {
+  const SubscriptionPlanScreen({Key? key}) : super(key: key);
+
+  @override
+  State<SubscriptionPlanScreen> createState() => _SubscriptionPlanScreenState();
+}
+
+class _SubscriptionPlanScreenState extends State<SubscriptionPlanScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Subscription List")),
+      body: PageView(
+        children: [
+          Container()
+        ],
+      ),
+    );
+  }
+}
